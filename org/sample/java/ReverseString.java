e05356b234643b453f8e705e26c3a14cf08ab333
deepusai <deepakjayaraam@gmail.com>
2019-08-05 18:27:58 +1000

    task1.

--- org/sample/java/ReverseString.java
+++ org/sample/java/ReverseString.java
@@ -3,7 +3,7 @@ package org.sample.java;
 public class ReverseString {
 
 	public static void main(String[] args) {
-		String str = "deepu";
+		String str = "green technology";
 		for(int i=str.length()-1;i>=0;i--) {
 			System.out.print(str.charAt(i));
 		}
---
+++ org/sample/java/WordCount.java
@@ -0,0 +1,11 @@
+package org.sample.java;
+
+public class WordCount {
+
+	public static void main(String[] args) {
+		String str = "Green  Technology  OMR";
+		System.out.println(str.split("\\s+").length);
+
+	}
+
+}
